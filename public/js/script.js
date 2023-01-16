fa78d6022dfa4d4c603bdce4198ed2cbea2ba8c5
tablieh <waledo.1997@hotmail.com>
2023-01-16 13:28:10 +0100

    parti event done

--- public/js/script.js
+++ public/js/script.js
@@ -34,3 +34,17 @@ tous.addEventListener("click", function(event){
         image.style.display="block"
     }
 })
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
